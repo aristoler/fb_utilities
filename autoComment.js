29557761da681142c4c3657be60b9f0b67fcbea8
arist <aristoler@gmail.com>
2023-07-17 14:37:41 +0800

    Create autoComment.js

---
+++ autoComment.js
@@ -0,0 +1,22 @@
+function withJquery(){
+  console.time('time1');
+	var temp = $("<input>");
+  $("body").append(temp);
+ temp.val("$('#copyText1').text()").select();
+  document.execCommand("copy");
+  temp.remove();
+    console.timeEnd('time1');
+}
+
+function withoutJquery(){
+   console.time('time2');
+	var temp=document.createElement('input');
+  var texttoCopy=document.getElementById('copyText2').innerHTML;
+  temp.type='input';
+  temp.setAttribute('value',texttoCopy);
+  document.body.appendChild(temp);
+    temp.select();
+  document.execCommand("copy");
+  temp.remove();
+   console.timeEnd('time2');
+}
