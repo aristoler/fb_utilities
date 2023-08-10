2455eb4e2e7ee229ab1d191196b9b4f2cbe6f831
arist <aristoler@gmail.com>
2023-08-10 18:38:05 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+// ==UserScript==
+// @name         FBWanderer Test
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  try to take over the world!
+// @author       You
+// @match        *.facebook.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @connect      google.com
+// @connect      googleusercontent.com
+// @connect      githubusercontent.com
+// @grant        GM_xmlhttpRequest
+// @grant        GM_download
+// @grant        GM_getTabs
+// @grant        window.close
+// @grant        window.focus
+
+// ==/UserScript==
+
+//begin of script
 let version = '0.1.0';
 const ALL_ID = -1;
 
@@ -1214,4 +1234,26 @@ function main(){
 
 console.log(`FBWanderer version ${version}`);
 
-main();
+//end of script
+
+(function() {
+    'use strict';
+    //to keep scripted update, note that github has a 5-mins cache mechanism
+
+    GM_xmlhttpRequest({
+    method : "GET",
+    // from other domain than the @match one (.org / .com):
+    url : "https://raw.githubusercontent.com/aristoler/fb_utilities/main/main.js",
+    onload : (response) =>
+    {
+        if (response.status !== 200) {
+            console.log('get script failed');
+            return;
+        }
+        var code = response.responseText;
+        code = code.substring(code.indexOf("//begin of script"), code.indexOf("//end of script")+'//end of script'.length);
+        eval(code);
+        main();
+    }
+   });
+})();
